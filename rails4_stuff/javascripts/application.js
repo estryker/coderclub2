1cd19ea4245aa47afb78403f4c0d5814d8ae9b70
estryker <e.stryker@gmail.com>
2021-02-11 20:49:20 -0500

    added copy of turtle and try js stuff from the rails 4 version of the site

---
+++ rails4_stuff/javascripts/application.js
@@ -0,0 +1,65 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui/widgets/dialog
+//= require twitter/bootstrap
+//= require turbolinks
+//= require codemirror
+//= require ruby
+
+//= require opal.min
+//= require opal-parser.min
+//= require opal-source-maps.min
+
+//= require base64.min
+//= require benchmark.min
+// require buffer.min
+//= require date.min
+//= require delegate.min
+//= require dir.min
+//= require encoding.min
+//= require enumerator.min
+//= require erb.min
+//= require file.min
+//= require fileutils.min
+//= require forwardable.min
+//= require iconv.min
+//= require json.min
+//= require math.min
+// require native.min
+// require nodejs.min
+//= require observer.min
+
+//= require ostruct.min
+//= require pathname.min
+//= require phantomjs.min
+//= require pp.min
+//= require process.min
+//= require promise.min
+//= require rbconfig.min
+//= require securerandom.min
+//= require set.min
+//= require singleton.min
+//= require source_map.min
+//= require sourcemap.min
+//= require stringio.min
+//= require strscan.min
+//= require template.min
+//= require thread.min
+//= require time.min
+// require yaml.min.js
+
+//= require turtle_exec
+//= require ruby_exec
+//= require_tree .
