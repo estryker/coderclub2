cddc930cb2589ef5796d3cfac6c7075f885bd21b
estryker <e.stryker@gmail.com>
2021-02-21 20:09:57 -0500

    trying the classic auto loader

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module Coderclub2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # because of error on heroku:  /app/vendor/bundle/ruby/2.7.0/gems/zeitwerk-2.4.2/lib/zeitwerk/loader/callbacks.rb:18:in `on_file_autoloaded': expected file /app/app/opal/opal_web_worker_loader.rb to define constant OpalWebWorkerLoader, but didn't (Zeitwerk::NameError)
+    # solution here: https://stackoverflow.com/questions/57277351/rails-6-zeitwerknameerror-doesnt-load-class-from-module
+    config.autoloader = :classic
   end
 end
